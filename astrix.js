759a7701a4b0a2712da49c707a82e49356e6f388
yossits <55515778+yossits@users.noreply.github.com>
2019-10-18 13:39:05 +0300

    Add files via upload

---
+++ astrix.js
@@ -0,0 +1,11 @@
+debugger
+function astrix() {
+    let rib = document.getElementById('astrix').value;
+    for (let y = 0; y < rib; y++) {
+        for (let x=0 ; x < rib; x++) {
+            if (x==y || y==(rib-1)-x || y==(rib-1)/2 || x==(rib-1)/2 ){document.write('*');}
+            else {document.write('#');}
+        }
+        document.write('<br>');
+    }
+}
